2d8501ea1eb363cae69ab141806d2fbe533e1404
nicdkim <zzsehdrb@naver.com>
2024-12-11 16:06:23 +0900

    docker

---
+++ src/main/java/com/itdat/back/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.itdat.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://www.namewallet.store") // 클라이언트 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
