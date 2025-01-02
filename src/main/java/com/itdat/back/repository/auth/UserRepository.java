ade2c041525d9c8ac7188bf91457f6dfbe847032
nicdkim <zzsehdrb@naver.com>
2025-01-02 10:06:11 +0900

    e

--- src/main/java/com/itdat/back/repository/auth/UserRepository.java
+++ src/main/java/com/itdat/back/repository/auth/UserRepository.java
@@ -1,5 +1,6 @@
 package com.itdat.back.repository.auth;
 
+import com.itdat.back.entity.auth.Role;
 import com.itdat.back.entity.auth.User;
 import com.itdat.back.entity.auth.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +11,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUserEmail(String email);
-    // User findByUserEmail(String email);
+//     Optional<User> findByUserEmail(String email);
+    User findByUserEmail(String email);
     List<User> findByRole(Role role);
     boolean existsByUserId(String userId);
     boolean existsByUserEmail(String email);
