316fde6401b020e53a1414572c32c837f7469262
kkandda <diobobo@naver.com>
2025-01-06 18:58:37 +0900

    qna 수정 로직 구현(엔티티 업데이트)

--- src/main/java/com/itdat/back/controller/qna/QnaController.java
+++ src/main/java/com/itdat/back/controller/qna/QnaController.java
@@ -34,14 +34,14 @@ public class QnaController {
     public ResponseEntity<Object> getAllQnaList() {
         try {
             List<Qna> allQnaList = qnaService.getAllQnaList();
-            if(allQnaList != null && !allQnaList.isEmpty()){
+            if (allQnaList != null && !allQnaList.isEmpty()) {
                 return ResponseEntity.ok(allQnaList);
-            }else {
+            } else {
                 return ResponseEntity.ok("아직 QnA 게시판에 게시된 글이 없습니다.");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR/*500, 500번대는 서버문제*/)
-                    .body("서버측에서 문제가 발생하였습니다. 에러: "+e.getMessage());
+                    .body("서버측에서 문제가 발생하였습니다. 에러: " + e.getMessage());
         }
     }
 
@@ -51,19 +51,19 @@ public class QnaController {
 
         try {
             Qna selectedQna = qnaService.findById(selectedId);
-            if(selectedQna != null){
+            if (selectedQna != null) {
                 return ResponseEntity.ok(selectedQna);
-            }else {
+            } else {
                 return ResponseEntity.ok("선택된 게시물의 정보가 비어있습니다.");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("서버측에서 문제가 발생하였습니다. "+e.getMessage());
+                    .body("서버측에서 문제가 발생하였습니다. " + e.getMessage());
         }
     }
 
     @PostMapping("/write")
-    public ResponseEntity<Object> writeQna(@RequestBody Map<String,Object> qnaData) {
+    public ResponseEntity<Object> writeQna(@RequestBody Map<String, Object> qnaData) {
         System.out.println("qnaData asd  = " + qnaData);
         User slectedUser = userRepository.findByUserId(qnaData.get("loginedUserId").toString());
 
@@ -75,11 +75,11 @@ public class QnaController {
         createdQna.setUpdateAt(LocalDateTime.now());
 //        System.out.println("qnaDTO.isSecret() = " + qnaDTO.isSecret());
         createdQna.setSecret((Boolean) qnaData.get("isSecret"));
-      if((Boolean) qnaData.get("isSecret")){
-          createdQna.setPassword(passwordEncoder.encode(qnaData.get("password").toString()));
-      }else {
-          createdQna.setPassword(null);
-      }
+        if ((Boolean) qnaData.get("isSecret")) {
+            createdQna.setPassword(passwordEncoder.encode(qnaData.get("password").toString()));
+        } else {
+            createdQna.setPassword(null);
+        }
 //        createdQna.setPassword(passwordEncoder.encode(qnaData.get("password").toString()));
         createdQna.setCategory(QnaCategory.valueOf(qnaData.get("category").toString()));
 
@@ -93,7 +93,7 @@ public class QnaController {
     }
 
     @PostMapping("/check-password")
-    public ResponseEntity<Object> checkPassword(@RequestBody Map<String,String> data) {
+    public ResponseEntity<Object> checkPassword(@RequestBody Map<String, String> data) {
         System.out.println("data = " + data);
         int selectedQnaId = Integer.parseInt(data.get("id"));
         String password = data.get("checkPassword");
@@ -103,14 +103,38 @@ public class QnaController {
         // 비밀번호 검증
         try {
             assert selectedQna != null; // selectedQna가 비어있지 않음을 보장!!
-            if(passwordEncoder.matches(password, selectedQna.getPassword())){
+            if (passwordEncoder.matches(password, selectedQna.getPassword())) {
                 return ResponseEntity.ok(true);
-            }else {
+            } else {
                 return ResponseEntity.ok(false);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateQna(@RequestBody Map<String, Object> qnaData) {
+        System.out.println("qnaData asd  = " + qnaData);
+        Qna selectedQna = qnaRepository.findById((Integer) qnaData.get("id")).orElse(null);
+
+        selectedQna.setTitle(qnaData.get("title").toString());
+        selectedQna.setContents(qnaData.get("contents").toString());
+        selectedQna.setUpdateAt(LocalDateTime.now());
+        selectedQna.setSecret((Boolean) qnaData.get("isSecret"));
+        if ((Boolean) qnaData.get("isSecret")) {
+            selectedQna.setPassword(passwordEncoder.encode(qnaData.get("password").toString()));
+        } else {
+            selectedQna.setPassword(null);
+        }
+        selectedQna.setCategory(QnaCategory.valueOf(qnaData.get("category").toString()));
+        try {
+            qnaRepository.save(selectedQna);
+            return ResponseEntity.ok("수정됐습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버측의 문제로 게시물이 저장되지 못 했습니다. " + e.getMessage());
+        }
+
     }
 
 }
