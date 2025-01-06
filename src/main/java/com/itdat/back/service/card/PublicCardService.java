8340d14bb3656bfa22d32925c563b83fef5d4951
nicdkim <zzsehdrb@naver.com>
2025-01-06 13:05:23 +0900

    open card

---
+++ src/main/java/com/itdat/back/service/card/PublicCardService.java
@@ -0,0 +1,29 @@
+package com.itdat.back.service.card;
+
+import com.itdat.back.entity.card.BusinessCard;
+import com.itdat.back.repository.card.BusinessCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PublicCardService {
+
+    @Autowired
+    private BusinessCardRepository businessCardRepository;
+
+    // 공개된 명함 가져오기
+    public List<BusinessCard> getAllPublicCards() {
+        return businessCardRepository.findByIsPublicTrue();
+    }
+
+    // 공개 여부 업데이트
+    public void updateCardVisibility(Integer cardId, boolean isPublic) {
+        BusinessCard card = businessCardRepository.findById(cardId)
+                .orElseThrow(() -> new RuntimeException("Card not found"));
+        card.setPublic(isPublic);
+        businessCardRepository.save(card);
+    }
+}
+
