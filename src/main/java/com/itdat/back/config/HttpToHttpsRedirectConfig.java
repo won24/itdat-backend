e4e260255d158c542c3f991081a495c87d169533
김 동규 <106741770+nicdkim@users.noreply.github.com>
2025-02-19 14:48:22 +0900

    https

--- src/main/java/com/itdat/back/config/HttpToHttpsRedirectConfig.java
+++ src/main/java/com/itdat/back/config/HttpToHttpsRedirectConfig.java
@@ -1,27 +1,28 @@
-//package com.itdat.back.config;
-//
-//import org.apache.catalina.connector.Connector;
-//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class HttpToHttpsRedirectConfig {
-//
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-//        factory.addAdditionalTomcatConnectors(createHttpConnector()); // HTTP를 HTTPS로 리디렉션
-//        return factory;
-//    }
-//
-//    private Connector createHttpConnector() {
-//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-//        connector.setScheme("http");
-//        connector.setPort(8081);  // HTTP 포트
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8082);  // HTTPS 포트
-//        return connector;
-//    }
-//}
+package com.itdat.back.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsRedirectConfig {
+
+   @Bean
+   public ServletWebServerFactory servletContainer() {
+       TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+       factory.addAdditionalTomcatConnectors(createHttpConnector()); // HTTP를 HTTPS로 리디렉션
+       return factory;
+   }
+
+   private Connector createHttpConnector() {
+       Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+       connector.setScheme("http");
+       connector.setPort(8081);  // HTTP 포트
+       connector.setPort(8080);  // HTTP 포트
+       connector.setSecure(false);
+       connector.setRedirectPort(8082);  // HTTPS 포트
+       return connector;
+   }
+}
